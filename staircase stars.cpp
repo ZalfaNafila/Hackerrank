be9b2df6e7f0e6cba89e9af7cfa2de041313c759
Zalfa Nafila Khairunnisa <92864261+ZalfaNafila@users.noreply.github.com>
2021-10-20 20:19:16 +0700

    a staircase..

---
+++ staircase stars.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n;
+	cin >> n;
+	
+	for (int j=0; j<n; j++){
+		for (int i=0; i<n-j-1; i++){
+		cout << " ";
+		}
+		for (int i=n-j; i<=n; i++){
+			cout << "#";
+		}
+	cout << "\n";
+	}
+}
