2beede747d0665425d18f28502e3be3fe0497b3b
Zalfa Nafila Khairunnisa <92864261+ZalfaNafila@users.noreply.github.com>
2021-10-20 20:11:04 +0700

    Add files via upload

---
+++ triplets2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int ascore = 0;
+	int bscore = 0;
+	int a[3], b[3];
+	
+	for (int i = 0; i<3; i++){
+		cin >> a[i];
+	}
+	for (int i=0; i<3; i++){
+		cin >> b[i];
+		if (a[i] > b[i]){
+			ascore+=1;
+		}
+		else if(b[i] > a[i]){
+			bscore+=1;
+		}
+	}	
+	cout << ascore << " " << bscore;
+}
+
+
+
+
