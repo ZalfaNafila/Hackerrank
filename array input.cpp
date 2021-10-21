07aaeb2db760c9ec1e305f8a3ee79a84cfbfae9d
Zalfa <92864261+ZalfaNafila@users.noreply.github.com>
2021-10-21 21:10:46 +0700

    Add files via upload

---
+++ array input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int arr[1001];
+	int n;
+	int sum = 0;
+	cin >> n;
+	for (int i=0; i<n; i++){
+		cin >> arr[i];
+}
+	for(int i=0; i<n; i++){
+			sum += arr[i];
+}
+		cout << sum;
+}
